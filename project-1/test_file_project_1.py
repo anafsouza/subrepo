5fa67f386000e329b6486823896595cb4c01caf3
Ana Flávia Souza <ana.fsouza.santos@gmail.com>
2025-07-28 15:06:43 -0300

    test: add docstring

--- project-1/test_file_project_1.py
+++ project-1/test_file_project_1.py
@@ -1,4 +1,12 @@
 def add(a, b):
+    """Compute the sum of two numbers.
+    Args:
+        a (int, float): The first number.
+        b (int, float): The second number.
+
+    Returns:
+        int, float: The sum of a and b.
+    """
     return a + b
 
 def subtract(a, b):
